b18f56b4f9d5a8cea0f33b348a5787bd144f7c98
kali <b1131683978@163.com>
2025-06-23 11:06:46 +0800

    update v1.0.2 add dialog

--- src/components/AllItemTable.vue
+++ src/components/AllItemTable.vue
@@ -30,8 +30,17 @@ const searchQuery = ref<string>('')
 const currentPage = ref<number>(1)
 const pageSize = ref('20')
 const filterStatus = ref('全部')
+const isVisible = ref(false)
+const lastVisit = localStorage.getItem('lastVisit')
+const daysThreshold = 3
 
 // ====== 计算属性 ======
+
+const handleClose = () => {
+  isVisible.value = false
+  localStorage.setItem('lastVisit', Date.now().toString())
+}
+
 const filteredItemList = computed(() => {
   let result = itemList.value
 
@@ -94,6 +103,17 @@ onMounted(() => {
       item.isCollection = collections[item.id] ?? false
     })
   }
+
+  if(lastVisit){
+    const diffTime = Math.abs(Date.now() - Number(lastVisit))
+    const diffDays = Math.ceil(diffTime / (1000 * 60 *60 *24))
+    if(diffDays >= daysThreshold){
+      isVisible.value = true
+    }
+  }else {
+    isVisible.value = true
+  }
+  console.log(isVisible)
 })
 
 watch(
@@ -171,6 +191,45 @@ const handleUpload = (uploadFile: any) => {
 </script>
 
 <template>
+  <el-dialog v-model="isVisible" width="500px" title="请仔细阅读以下文字" center>
+    <span>
+       温馨提示：为了获得最佳体验，请前往网盘检查是否发布了最新版本。
+      <br/>
+      <br/>
+      此版本为<span style="color:Red">1.0.2</span>版本
+      <br/>
+      <br/>
+      支持了以下功能：
+      <br/>
+      上传表格同步数据
+      <br/>
+      根据是否收集分类
+      <br/>
+      <br/>
+      <a class="a-class" href="https://wwut.lanzoul.com/b00csc983e" target="_blank">点击这里访问网盘</a>
+      <br/>
+      密码:ezbe
+      <br/>
+      <br/>
+      <br/>
+      特别鸣谢：<a class="a-class" href="https://space.bilibili.com/2075535" target="_blank">@404岛主</a> 提供原始数据
+      <br/>
+      <br/>
+      欢迎通过各种渠道提出建议~
+      <br/>
+      bilibili : <a class="a-class" href="https://space.bilibili.com/3493289458141747" target="_blank">@可丽饼kali</a>
+      <br/>
+      小黑盒 : <a class="a-class" href="https://www.xiaoheihe.cn/app/user/profile/21709567" target="_blank">@可丽饼kali</a>
+    </span>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button type="primary" @click="handleClose">
+          确定
+        </el-button>
+      </span>
+    </template>
+  </el-dialog>
+
   <el-container style="height: calc(100vh - 60px); overflow-y: auto; align-items: center;">
     <el-header style="display: flex; align-items: center; justify-content: space-between; background-color: #ffffff; padding: 0 20px; width: 100%">
 
@@ -189,7 +248,7 @@ const handleUpload = (uploadFile: any) => {
               placement="top"
           >
             <template #content>
-              请上传 <a style="text-decoration: none; color: SkyBlue;" href="https://space.bilibili.com/2075535" target="_blank">@404岛主</a> 发布的【泰拉瑞亚1449全收集列表】
+              请上传 <a class="a-class" href="https://space.bilibili.com/2075535" target="_blank">@404岛主</a> 发布的【泰拉瑞亚1449全收集列表】
               <br/>
               解析数据需要花费时间较长,请耐心等待
             </template>
@@ -301,6 +360,11 @@ const handleUpload = (uploadFile: any) => {
   width: auto;
   height: auto;
   object-fit: contain;
+}
+
+.a-class {
+  text-decoration: none;
+  color: SkyBlue;
 }
 
 .el-header {
